2177ff73caf14fa5c9b06cbd730ab5d4f2e89f48
u8y7541 <surya.subbarao1@gmail.com>
2022-06-21 20:11:19 -0700

    Added counting sort

--- radix_sort/counting_sort.cpp
+++ radix_sort/counting_sort.cpp
@@ -1,10 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Unstable sort
 void counting_sort(int* A, int n, int k) {
-	int C[k+1] = { 0 };
-	for (int i = 0; i<n; i++) C[A[i]+1]++;
-	for (int i = 1; i<k; i++) C[i] += C[i-1];
-	for (int i = 0; i<k; i++) {
-		A[k]
+	int C[k] = { 0 };
+	int i = 0;
+	for (; i<n; i++) C[A[i]]++;
+	i = 0;
+	for (int j = 0; j<k; j++) for (int l = 0; l<C[j]; l++) A[i++] = j;
+}
+
+int main(void) {
+	int A[10] = {5,0,4,0,0,2,3,3,1,4};
+	cout<<"Unsorted: ";
+	for (int i = 0; i<10; i++) cout<<A[i]<<' '; cout<<endl;
+	counting_sort(A, 10, 6);
+	cout<<"Sorted: ";
+	for (int i = 0; i<10; i++) cout<<A[i]<<' '; cout<<endl;
+
+	return 0;
 }
